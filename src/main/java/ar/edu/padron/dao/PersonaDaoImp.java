67cc93e86808f82504817635bacdb36586a76180
maximiliano-occhiuzzi <omaximiliano969@gmail.com>
2026-04-26 22:23:34 -0300

    Arquitectura base: ConexionDB, POJOs e Interfaces DAO

--- src/main/java/ar/edu/padron/dao/PersonaDaoImp.java
+++ src/main/java/ar/edu/padron/dao/PersonaDaoImp.java
@@ -1,5 +1,51 @@
 package ar.edu.padron.dao;
 
-public class PersonaDaoImp {
+import java.util.List;
+
+import ar.edu.padron.domain.Persona;
+
+public class PersonaDaoImp implements PersonaDAO {
+
+	@Override
+	public int save(Persona p) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Persona findByDocumento(String dni) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Persona> findAll() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean existeDocumento(String dni) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateHabilitado(int id, boolean habilitado) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean saveImagen(int personaId, byte[] img, String nombre) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public byte[] getImagen(int personaId) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/main/java/ar/edu/padron/dao/UsuarioDaoImp.java
+++ src/main/java/ar/edu/padron/dao/UsuarioDaoImp.java
@@ -1,5 +1,19 @@
 package ar.edu.padron.dao;
 
-public class UsuarioDaoImp {
+import ar.edu.padron.domain.Usuario;
+
+public class UsuarioDaoImp implements UsuarioDAO{
+
+	@Override
+	public Usuario findByUsername(String user) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean validarCredenciales(String user, String pass) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
