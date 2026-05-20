2a6ea7065b4866a3f070e763b1427ef8e0676d7d
Sebastiianpz <sebastiandavalos09@gmail.com>
2026-05-20 10:39:39 -0300

    Archivos test

---
+++ src/main/java/com/sample/core/domain/TesterController.java
@@ -0,0 +1,20 @@
+package com.sample.core.domain;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class TesterController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.getWriter().println("Dockerizado");
+    }
+}
